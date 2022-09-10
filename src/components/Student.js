48513e246fb3cdad1c2285b007580d9bdcddbbb1
vrushtikaria <20it052@charusat.edu.in>
2022-09-11 02:01:24 +0530

    Implement Student Save

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,31 +1,66 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react";
 import { Container, Form, Card, Button } from "react-bootstrap";
-import CardHeader from "react-bootstrap/esm/CardHeader";
+
 
 export default function Student() {
+
+  const [id, setId] = useState();
+  const [name, setName] = useState();
+  const [address, setAddress] = useState();
+
+  let student = {
+    id : id,
+    name : name,
+    address : address
+  }
+
+  let textChanged = (event) => {
+    if(event.target.name==="id"){
+      setId(event.target.value);
+    } else if(event.target.name==="name"){
+      setName(event.target.value);
+    } else if(event.target.name==="address"){
+      setAddress(event.target.value);
+    }
+  }
+
+  let saveStudent = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:8080/student", student)
+    .then(response => {
+      if(response.data != null){
+        alert('Record added successfully');        
+      }
+    })
+    .catch(error => alert(error));
+  }
+
   return (
     <div  className="my-3">
     <Container>
       <Card>
-        <Form>
+        <Form onSubmit={saveStudent}>
           <Card.Header>Add Student Information</Card.Header>
           <Card.Body>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Id</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control name="id" value={id} type="text" placeholder="Enter id" onChange={textChanged}/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control name="name" value={name} type="text" placeholder="Enter name" onChange={textChanged} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Label>Address</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control name="address" value={address} type="text" placeholder="Enter address" onChange={textChanged} />
             </Form.Group>
           </Card.Body>
+          <Card.Footer>
           <Button variant="primary" type="submit">
             Submit
           </Button>
+          </Card.Footer>
         </Form>
       </Card>
     </Container>
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import { Container, Table } from 'react-bootstrap'
+import React from "react";
+import { Card, Container, Table } from "react-bootstrap";
 
 export default function StudentList() {
   return (
     <div className="my-3">
-        <Container>
-        <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Student Id</th>
-          <th>Student Name</th>
-          <th>Student Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-        </Container>
+      <Container>
+        <Card.Header><h3>Students List</h3></Card.Header>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Container>
     </div>
-  )
+  );
 }
