1e66586ea9843db2c4124792b67b2ee8a831ba1c
vrushtikaria <20it052@charusat.edu.in>
2022-09-11 02:02:13 +0530

    Added Navigationbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <Student/>
       <StudentList/>
     </div>
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function NavigationBar() {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Add Student</Nav.Link>
+            <Nav.Link href="#pricing">View Students</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
