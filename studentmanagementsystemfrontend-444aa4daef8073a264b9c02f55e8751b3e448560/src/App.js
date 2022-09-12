dcb875ffe71abf463469f7b4dcfbc4b58d82fef6
vrushtikaria <20it052@charusat.edu.in>
2022-09-12 12:50:51 +0530

    Added Edit/Delete Buttons to Students Listing.

--- studentmanagementsystemfrontend-444aa4daef8073a264b9c02f55e8751b3e448560/src/App.js
+++ studentmanagementsystemfrontend-444aa4daef8073a264b9c02f55e8751b3e448560/src/App.js
@@ -4,13 +4,17 @@ import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
+      
       <NavigationBar/>
+      <Container>
       <Student/>
       <StudentList/>
+      </Container>
       <Footer/>
     </div>
   );
